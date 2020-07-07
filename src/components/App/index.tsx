4cc758aea149411866e8807faf070b1f5aabf213
0B1N <binnyy01@gmail.com>
2020-07-07 11:28:57 +0900

    Initial Commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import "./index.scss";
+
+interface IApp {
+}
+
+
+const App: React.FunctionComponent<IApp> = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+export default App;
